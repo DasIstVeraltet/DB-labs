73ba7e8c9176cea171b696ffceed833e2d6558d7
DasIstVeraltet <standalong228@gmail.com>
2020-10-04 00:29:54 +0300

    lab3

---
+++ lab3/V9_1.sql
@@ -0,0 +1,124 @@
+﻿use AdventureWorks2012;
+go
+
+/* 
+добавьте в таблицу dbo.StateProvince поле AddressType типа nvarchar(50);
+*/
+
+alter table dbo.StateProvice 
+add AddressType nvarchar(50);
+go
+
+/* 
+объявите табличную переменную с такой же структурой как dbo.StateProvince и 
+заполните ее данными из dbo.StateProvince. Поле AddressType заполните данными 
+из таблицы Person.AddressType поля Name;
+*/
+
+declare @dboStateProvice table (
+	StateProvinceID int not null,
+	StateProvinceCode nchar(3) not null,
+	CountryRegionCode nvarchar(3) not null,
+	IsOnlyStateProvince smallint,
+	Name nvarchar(50) not null,
+	TerritoryID int not null,
+	rowguid uniqueidentifier not null,
+	ModifiedDate datetime not null,
+	AddressType nvarchar(50));
+
+insert into @dboStateProvice (
+	StateProvinceID,
+	StateProvinceCode,
+	CountryRegionCode,
+	IsOnlyStateProvince,
+	Name,
+	TerritoryID,
+	rowguid,
+	ModifiedDate,
+	AddressType)
+select 
+	sp.StateProvinceID,
+	sp.StateProvinceCode,
+	sp.CountryRegionCode,
+	sp.IsOnlyStateProvince,
+	sp.Name,
+	sp.TerritoryID,
+	sp.rowguid,
+	sp.ModifiedDate,
+	at.Name
+ from dbo.StateProvice as sp
+ inner join Person.Address as a
+ on sp.StateProvinceID = a.StateProvinceID
+ inner join Person.BusinessEntityAddress as bea
+ on a.AddressID = bea.AddressID
+ inner join Person.AddressType as at
+ on bea.AddressTypeID = at.AddressTypeID;
+
+ select * from @dboStateProvice;
+
+
+/* 
+обновите поле AddressType в dbo.StateProvince данными из табличной переменной, 
+добавьте в начало названия каждого штата в поле Name название региона из 
+Person.CountryRegion;
+*/
+ 
+ update dbo.StateProvice
+ set AddressType = varsp.AddressType,
+	 Name = (cr.Name + ' ' + sp.Name)	
+ from dbo.StateProvice as sp 
+ inner join @dboStateProvice as varsp
+ on sp.StateProvinceID = varsp.StateProvinceID
+ inner join Person.CountryRegion as cr
+ on sp.CountryRegionCode = cr.CountryRegionCode;
+ go
+
+ select * from dbo.StateProvice;
+ go
+
+ /* 
+ удалите данные из dbo.StateProvince, оставив по одной строке для каждого 
+ значения из AddressType с максимальным StateProvinceID;
+ */
+
+ delete from dbo.StateProvice 
+ where StateProvinceID not in (
+	select 
+		max(StateProvinceID) as StateProvinceID 
+	from dbo.StateProvice 
+	group by AddressType);
+ go
+
+ select * from dbo.StateProvice;
+ go
+
+ /*
+ удалите поле AddressType из таблицы, удалите все созданные 
+ ограничения и значения по умолчанию.
+ */
+
+ alter table dbo.StateProvice
+ drop column AddressType;
+ go
+
+ select * from dbo.StateProvice;
+ go
+
+ alter table dbo.StateProvice
+ drop constraint CK__StateProv__Terri__269AB60B;
+ go
+
+ alter table dbo.StateProvice
+ drop constraint DF_TerritoryID;
+ go
+
+ /*
+ удалите таблицу dbo.StateProvince.
+ */
+
+ drop table dbo.StateProvice;
+ go
+
+ select * from dbo.StateProvice;
+ go
+
---
+++ lab3/V9_2.sql
@@ -0,0 +1,165 @@
+﻿/* 
+выполните код, созданный во втором задании второй лабораторной работы. 
+Добавьте в таблицу dbo.StateProvince поля TaxRate SMALLMONEY, CurrencyCode NCHAR(3) и 
+AverageRate MONEY. Также создайте в таблице вычисляемое поле IntTaxRate, 
+округляющее значение в поле TaxRate в большую сторону до ближайшего целого.
+*/
+
+ alter table dbo.StateProvice
+ add TaxRate smallmoney;
+ go
+
+ alter table dbo.StateProvice
+ add CurrencyCode nchar(3);
+ go
+
+ alter table dbo.StateProvice
+ add AverageRate money;
+ go
+
+ alter table dbo.StateProvice
+ add IntTaxRate as ceiling(TaxRate);
+ go
+
+ select * from dbo.StateProvice;
+ go
+
+ /* 
+ создайте временную таблицу #StateProvince, с первичным ключом по полю StateProvinceID. 
+ Временная таблица должна включать все поля таблицы dbo.StateProvince за исключением поля 
+ IntTaxRate.
+ */
+
+ create table #StateProvince (
+	StateProvinceID int not null,
+	StateProvinceCode nchar(3) not null,
+	CountryRegionCode nvarchar(3) not null,
+	IsOnlyStateProvince smallint,
+	Name nvarchar(50) not null,
+	TerritoryID int not null,
+	rowguid uniqueidentifier not null,
+	ModifiedDate datetime not null,
+	TaxRate smallmoney,
+	CurrencyCode nchar(3),
+	AverageRate money);
+ go
+
+ alter table #StateProvince
+ add constraint PK_StateProvince_StateProvinceID primary key (StateProvinceID);
+ go
+
+ /* 
+ заполните временную таблицу данными из dbo.StateProvince. 
+ Поле CurrencyCode заполните данными из таблицы Sales.Currency. 
+ Поле TaxRate заполните значениями налоговой ставки к розничным сделкам (TaxType = 1) 
+ из таблицы Sales.SalesTaxRate. Если для какого-то штата налоговая ставка не найдена, 
+ заполните TaxRate нулем. Определите максимальное значение курса обмена валюты (AverageRate) 
+ в таблице Sales.CurrencyRate для каждой валюты (указанной в поле ToCurrencyCode) и 
+ заполните этими значениями поле AverageRate. Определение максимального курса для 
+ каждой валюты осуществите в Common Table Expression (CTE).
+ */
+
+ with AverageRates (CurrencyCode, MaxAverageRate) as (
+	select cr.ToCurrencyCode, max(cr.AverageRate)
+	from Sales.CurrencyRate as cr
+	group by cr.ToCurrencyCode)
+ insert into #StateProvince (
+	StateProvinceID,
+	StateProvinceCode,
+	CountryRegionCode,
+	IsOnlyStateProvince,
+	Name,
+	TerritoryID,
+	rowguid,
+	ModifiedDate,
+	TaxRate,
+	CurrencyCode,
+	AverageRate)
+ select 
+	sp.StateProvinceID,
+	sp.StateProvinceCode,
+	sp.CountryRegionCode,
+	sp.IsOnlyStateProvince,
+	sp.Name,
+	sp.TerritoryID,
+	sp.rowguid,
+	sp.ModifiedDate,
+	isnull(tr.TaxRate, 0),
+	crc.CurrencyCode,
+	ar.MaxAverageRate
+ from dbo.StateProvice as sp
+ inner join Sales.CountryRegionCurrency as crc
+ on sp.CountryRegionCode = crc.CountryRegionCode
+ inner join Sales.SalesTaxRate as tr
+ on sp.StateProvinceID = tr.StateProvinceID and tr.TaxType = 1
+ inner join AverageRates as ar
+ on crc.CurrencyCode = ar.CurrencyCode;
+ go
+
+ select * from #StateProvince;
+ go
+
+ /* 
+ удалите из таблицы dbo.StateProvince строки, где CountryRegionCode=’CA’
+ */
+
+  select * from dbo.StateProvice
+  where CountryRegionCode = 'CA';
+  go
+
+  delete from dbo.StateProvice
+  where CountryRegionCode = 'CA';
+  go
+
+  select * from dbo.StateProvice
+  where CountryRegionCode = 'CA';
+  go
+
+  /*
+  напишите Merge выражение, использующее dbo.StateProvince как target, 
+  а временную таблицу как source. Для связи target и source используйте StateProvinceID. 
+  Обновите поля TaxRate, CurrencyCode и AverageRate, если запись присутствует в source и 
+  target. Если строка присутствует во временной таблице, но не существует в target, 
+  добавьте строку в dbo.StateProvince. Если в dbo.StateProvince присутствует такая строка, 
+  которой не существует во временной таблице, удалите строку из dbo.StateProvince.
+  */
+
+   merge dbo.StateProvice t 
+   using #StateProvince s
+   on (t.StateProvinceID = s.StateProvinceID)
+   when matched 
+		then update set 
+			t.TaxRate = s.TaxRate, 
+			t.CurrencyCode = s.CurrencyCode,
+			t.AverageRate = s.AverageRate
+   when not matched by target
+		then insert (
+			StateProvinceID,
+			StateProvinceCode,
+			CountryRegionCode,
+			IsOnlyStateProvince,
+			Name,
+			TerritoryID,
+			rowguid,
+			ModifiedDate,
+			TaxRate,
+			CurrencyCode,
+			AverageRate) 
+		values (
+			s.StateProvinceID,
+			s.StateProvinceCode,
+			s.CountryRegionCode,
+			s.IsOnlyStateProvince,
+			s.Name,
+			s.TerritoryID,
+			s.rowguid,
+			s.ModifiedDate,
+			s.TaxRate,
+			s.CurrencyCode,
+			s.AverageRate)
+   when not matched by source
+		then delete;
+   go
+
+   select * from dbo.StateProvice;
+   go
