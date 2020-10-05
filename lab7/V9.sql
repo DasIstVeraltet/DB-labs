37b03c45e71d210d72d8bc995aa86a132988fb17
DasIstVeraltet <standalong228@gmail.com>
2020-10-05 17:50:27 +0300

    lab7

---
+++ lab7/V9.sql
@@ -0,0 +1,23 @@
+﻿ 
+ /*
+ Вывести значения полей [StartDate], [EndDate]из таблицы [HumanResources].[EmployeeDepartmentHistory] и 
+ полей [GroupName] и [Name] из таблицы [HumanResources].[Department] в виде xml, сохраненного в переменную. 
+ 
+ */
+ declare @xml xml; 
+ 
+ set @xml = (
+	 select top 2
+		edh.StartDate as [Start],
+		edh.EndDate as [End],
+		d.GroupName as [Department/Group],
+		d.Name as [Department/Name]
+	 from HumanResources.EmployeeDepartmentHistory as edh
+	 inner join HumanResources.Department d
+	 on edh.DepartmentID = d.DepartmentID
+	 for xml path ('Transaction'), root('History')
+ );
+
+ select @xml;
+ go
+
